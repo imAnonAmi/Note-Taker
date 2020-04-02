162a4e945d4aeb431c90e5c4454afb243673b342
imAnonAmi <mcintireaw@gmail.com>
2020-04-02 12:11:31 -0700

    Cleaned up HTML route path messiness.

--- Develop/server.js
+++ Develop/server.js
@@ -11,13 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 //Set Routes
 
 //HTML
+//* The following HTML routes should be created:
+//  * GET `/notes` - Should return the `notes.html` file.
+//  * GET `*` - Should return the `index.html` file
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
   // If no matching route is found default to index
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 //API
